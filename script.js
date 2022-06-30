542965b0d3b516206b4618cb69385fc7df37eda5
Akash-Ramjyothi <akash.ramjyothi@gmail.com>
2022-06-30 15:49:45 +0530

    Completed Landing Page

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-var acc = document.getElementsByClassName("modules");
-var i;
+const accordion = document.getElementsByClassName('accordioncall');
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      } 
-    });
-  }
-
-  console.log("Module Clicked")
+for (i = 0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function(){
+    this.classList.toggle('active')
+    console.log("Clicked by user")
+  })
+}
